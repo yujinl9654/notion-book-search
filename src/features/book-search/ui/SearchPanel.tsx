0982edaae0a80725f9dda4c62b4f7fe849d7bda9
yujin.lee <yujinl9654@gmail.com>
2026-04-26 19:24:06 +0900

    [impv] 도서 검색 placeholder 문구 수정

--- src/features/book-search/ui/SearchPanel.tsx
+++ src/features/book-search/ui/SearchPanel.tsx
@@ -80,7 +80,7 @@ export default function SearchPanel({
           id="book-search-input"
           name="query"
           onChange={onChange}
-          placeholder="github"
+          placeholder="도서 제목, 저자, ISBN을 입력해 주세요"
           value={inputValue}
         />
       </div>
